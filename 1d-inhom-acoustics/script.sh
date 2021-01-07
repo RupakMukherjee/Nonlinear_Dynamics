33420fd3451c1684a2672313d9a8fa02664a48db
Rupak Mukherjee <rupakmukherjee01@gmail.com>
2021-01-07 12:40:36 -0500

    script-file-updated-to-latest

--- 1d-inhom-acoustics/script.sh
+++ 1d-inhom-acoustics/script.sh
@@ -4,6 +4,12 @@ sigma[1]=0.2
 sigma[2]=0.3
 sigma[3]=0.4
 sigma[4]=0.5
+sigma[5]=0.6
+sigma[6]=0.7
+sigma[7]=0.8
+sigma[8]=0.9
+sigma[9]=1.0
+
 epsilon[0]=0.01
 epsilon[1]=0.02
 epsilon[2]=0.03
@@ -14,27 +20,32 @@ k[1]=2
 k[2]=3
 k[3]=4
 k[4]=5
+k[5]=6
+k[6]=7
+k[7]=8
+k[8]=9
+k[9]=10
 
 sed -i -e "7s/.*/sigma0=${sigma[0]}/" avSlopePlot.py
-sed -i -e "8s/.*/sigmaN=${sigma[4]}/" avSlopePlot.py
+sed -i -e "8s/.*/sigmaN=${sigma[9]}/" avSlopePlot.py
 sed -i -e "9s/.*/epsilon0=${epsilon[0]}/" avSlopePlot.py
 sed -i -e "10s/.*/epsilonN=${epsilon[4]}/" avSlopePlot.py
 sed -i -e "11s/.*/k0=${k[0]}/" avSlopePlot.py
-sed -i -e "12s/.*/kN=${k[3]}/" avSlopePlot.py
-sed -i -e "13s/.*/Nx=5/" avSlopePlot.py
+sed -i -e "12s/.*/kN=${k[9]}/" avSlopePlot.py
+sed -i -e "13s/.*/Nx=10/" avSlopePlot.py
 sed -i -e "14s/.*/Ny=5/" avSlopePlot.py
-sed -i -e "15s/.*/Nz=5/" avSlopePlot.py
+sed -i -e "15s/.*/Nz=10/" avSlopePlot.py
 
-for i in {0..4}
+for i in {0..9}
 do
   sed -i -e "34s/.*/    sigma=${sigma[i]}/" alfven.py
   for j in {0..4}
   do
     sed -i -e "36s/.*/    epsilon=${epsilon[j]}/" alfven.py
-    for kappa in {0..4}
+    for kappa in {0..9}
     do
       sed -i -e "37s/.*/    k=${k[kappa]}/" alfven.py
-      for iter in {0..100}
+      for iter in {0..10}
       do
         python alfven.py && python energy.py
       done
